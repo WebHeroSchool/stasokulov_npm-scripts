e22cc10a9577d5d23178aee2231e560cdfa066bb
Stanislav Okulov <stasokulov@gmail.com>
2019-02-21 22:01:59 +0300

    Задание 8

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('js', () => {
     return gulp.src('source/*js')
+    .pipe(concat('index.js'))
     .pipe(babel({
         presets: ['@babel/env']
     }))
+    .pipe(uglify())
     .pipe(gulp.dest('target'));
 });
 
