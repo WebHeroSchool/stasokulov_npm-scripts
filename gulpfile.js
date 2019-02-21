50feeed0ab07a7af76fedd4ac563a6008451768a
Stanislav Okulov <stasokulov@gmail.com>
2019-02-21 21:42:39 +0300

    Задание 7

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+
 gulp.task('js', () => {
     return gulp.src('source/*js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(gulp.dest('target'));
 });
 
