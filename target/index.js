3e4ef2e43b16d954afa2d3b0b1812b7f2a73efcc
Stanislav Okulov <stasokulov@gmail.com>
2019-02-21 23:09:47 +0300

    Задание 11

--- target/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var a=3,b=8;
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QuanMiLCJ0ZXN0MS5qcyJdLCJuYW1lcyI6WyJhIiwiYiJdLCJtYXBwaW5ncyI6ImFBQUEsSUFBQUEsRUFBQSxFQ0FBQyxFQUFBIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsibGV0IGEgPSAzOyIsImxldCBiID0gODsiXX0=
