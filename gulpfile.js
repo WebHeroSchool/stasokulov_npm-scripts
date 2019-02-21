01c1f1e9df9128f6af35c27c1a02883a5b194e57
Stanislav Okulov <stasokulov@gmail.com>
2019-02-21 22:23:47 +0300

    Задание 10

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['js', 'css']);
 
 gulp.task('js', () => {
     return gulp.src('source/*js')
-    .pipe(concat('index.js'))
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    .pipe(uglify())
+    .pipe(sourcemaps.init())
+        .pipe(concat('index.js'))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())    
     .pipe(gulp.dest('target'));
 });
 
 gulp.task('css', () => {
     return gulp.src('source/*css')
-    .pipe(concat('index.css'))
-    .pipe(cssnano())
+    .pipe(sourcemaps.init())
+        .pipe(concat('index.css'))
+        .pipe(cssnano())
+    .pipe(sourcemaps.write()) 
     .pipe(gulp.dest('target'));
 });
